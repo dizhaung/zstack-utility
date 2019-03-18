1ed782cd9fd5363e67f4fb0144688664651b0a8d
Wei Wang <wei.wang@zstack.io>
2019-03-18 12:55:46 +0800

    Implement mini storage

--- kvmagent/kvmagent/plugins/vm_plugin.py
+++ kvmagent/kvmagent/plugins/vm_plugin.py
@@ -5199,6 +5199,7 @@ class VmPlugin(kvmagent.KvmAgent):
                 logger.warn("got excetion: %s" % e)
 
             if fixed is True:
+                logger.debug("resume vm %s" % dom.name)
                 vm.resume()
                 touchQmpSocketWhenExists(vm_uuid)
 
